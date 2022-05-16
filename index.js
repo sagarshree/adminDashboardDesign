f8e2c208a2a61b0c4bfece56b993baee07ad35a1
sagarshree <shreesagar48@gmail.com>
2022-05-16 16:16:20 +0545

    completed

---
+++ index.js
@@ -0,0 +1,96 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+
+menuBtn.addEventListener("click", () => {
+  sideMenu.style.display = "block";
+});
+closeBtn.addEventListener("click", () => {
+  sideMenu.style.display = "none";
+});
+themeToggler.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme-variables");
+  // document.documentElement.className="dark-theme-variables";
+  themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+  themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+});
+
+
+const Orders = [
+    {
+        productName:"Foldable Mini Table",
+        productNumber: "1",
+        paymentStatus: "Paid",
+        shipping:"Pending",
+    },
+    {
+        productName:"Foldable Mini Table",
+        productNumber: "1",
+        paymentStatus: "Paid",
+        shipping:"Pending",
+    },
+    {
+        productName:"Foldable Mini Table",
+        productNumber: "1",
+        paymentStatus: "Paid",
+        shipping:"Declined",
+    },
+    {
+        productName:"Foldable Mini Table",
+        productNumber: "1",
+        paymentStatus: "Paid",
+        shipping:"Pending",
+    },
+    {
+        productName:"Foldable Mini Table",
+        productNumber: "1",
+        paymentStatus: "Paid",
+        shipping:"Pending",
+    },
+    {
+        productName:"Foldable Mini Table",
+        productNumber: "1",
+        paymentStatus: "Paid",
+        shipping:"Pending",
+    },
+    {
+        productName:"Foldable Mini Table",
+        productNumber: "1",
+        paymentStatus: "Paid",
+        shipping:"Delivered",
+    },
+    {
+        productName:"Foldable Mini Table",
+        productNumber: "1",
+        paymentStatus: "Paid",
+        shipping:"Pending",
+    },
+    {
+        productName:"Foldable Mini Table",
+        productNumber: "1",
+        paymentStatus: "Paid",
+        shipping:"Pending",
+    },
+    
+];
+
+Orders.forEach((order) => {
+  const tr = document.createElement("tr");
+  const trContent = `
+        <td>${order.productName}</td>
+        <td>${order.productNumber}</td>
+        <td>${order.paymentStatus}</td>
+        <td class=${
+          order.shipping === "Declined"
+            ? "danger"
+            : order.shipping === "Pending"
+            ? "warning"
+            : "primary"
+        }>${order.shipping}</td>
+        <td class="primary">Details</td>
+    `;
+    tr.innerHTML = trContent;
+    document.querySelector("table tbody").appendChild(tr);
+});
+
